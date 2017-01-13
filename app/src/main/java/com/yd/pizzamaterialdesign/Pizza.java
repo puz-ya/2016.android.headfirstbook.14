151e752b12f4472f5a674e4ca158905d3c13c744
puz_ya <puzino.y.a@gmail.com>
2017-01-13 18:36:17 +0300

    Added support libraries; Added images; Added data-class (not SQL now).

---
+++ app/src/main/java/com/yd/pizzamaterialdesign/Pizza.java
@@ -0,0 +1,28 @@
+package com.yd.pizzamaterialdesign;
+
+/**
+ * Created by YD on 13.01.2017.
+ */
+
+public class Pizza {
+
+    private String mName;
+    private int mImageID;
+
+    private static final Pizza[] mPizzas = {
+        new Pizza("Diavolo", R.drawable.diavolo),
+        new Pizza("Funhi", R.drawable.funghi)
+    };
+
+    public Pizza(String s, int i){
+        mName = s;
+        mImageID = i;
+    }
+
+    public String getName() {
+        return mName;
+    }
+    public int getImageResourceId() {
+        return mImageID;
+    }
+}
